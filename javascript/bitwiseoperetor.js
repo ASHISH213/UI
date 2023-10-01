8a714b27164c05cf3c0bbbf7e92ba620e7e522a0
ASHISH213 <rajshsh66@gmail.com>
2023-10-01 18:09:14 +0530

    new file:   javascript/bitwiseoperetor.js 	new file:   javascript/if.js

---
+++ javascript/bitwiseoperetor.js
@@ -0,0 +1,25 @@
+
+var a = 6;
+var b = 1;
+
+// AND Operation
+console.log("A & B = " + (a & b));
+
+// OR operation
+console.log("A | B = " + (a | b));
+
+// xOR operation
+console.log("A ^ B = " + (a ^ b));
+
+
+// NOT operation
+console.log("~A = " + (~a));
+
+// Sign Propagating Right Shift
+console.log("A >> B = " + (a >> b));
+
+// Zero Fill Right Shift
+console.log("A >>> B = " + (a >>> b));
+
+// Left Shift
+console.log("A << B = " + (a << b));
---
+++ javascript/if.js
@@ -0,0 +1,13 @@
+//write  the function  find the number is even or not
+
+
+function findNumberIsEven(num){
+
+    if(num % 2 == 0){
+        console.log("given number is even");
+
+    }
+
+}
+
+findNumberIsEven(1001);
