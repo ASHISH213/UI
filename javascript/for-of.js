9f0e7e5a4e266f617688c524e40c3af39da50431
ASHISH213 <rajshsh66@gmail.com>
2023-10-02 14:53:29 +0530

    new file:   javascript/dom/index.html 	new file:   javascript/dom/index.js 	new file:   javascript/for-of.js 	new file:   javascript/forwithin.js

---
+++ javascript/for-of.js
@@ -0,0 +1,24 @@
+var arr=[10,20,30,40,50];
+
+var obj={
+    name:"ashish",
+    id:101,
+    city:"hyd"
+}
+
+for (var x of arr) {
+    console.log(x);
+    
+}
+
+
+for (var y in arr) {
+
+    console.log(y);
+    
+}
+
+for (var z of obj) {
+    console.log(z);
+    
+}
---
+++ javascript/forwithin.js
@@ -0,0 +1,12 @@
+var obj={
+    name:"ashish",
+    id:101,
+    city:"hyd"
+};
+
+for (var x in obj) {
+
+    console.log(x);
+    console.log(x ,":", obj[x]);
+   
+}
